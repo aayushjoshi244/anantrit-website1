e54f3ba2c07f502c7c1c0943ca48eaa1e9712ddf
aayushjoshi244 <aayush09345@gmail.com>
2025-02-05 21:09:59 +0530

    update the xora with Anantrit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -70,9 +70,9 @@ const Header = () => {
                     )}
                   >
                     <img
-                      src="/images/xora.svg"
-                      width={160}
-                      height={55}
+                      src="/images/anantrit.svg"
+                      width={70}
+                      height={45}
                       alt="logo"
                     />
                   </LinkScroll>
